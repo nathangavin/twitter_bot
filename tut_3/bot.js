6e59acc248bebf15d83ab698cad2ef80a6803136
nathan gavin <nathangavin987@gmail.com>
2019-02-26 21:06:19 +1300

    implemented basic twitter api calling, with keys not included in commit. config_empty has instructions on how to produce keys.

--- tut_3/bot.js
+++ tut_3/bot.js
@@ -1,1 +1,8 @@
+let Twit = require('twit');
+let config = require('./config');
+
 console.log("hello");
+
+// making a new Twit object
+// keys are unique to my test account
+let T = new Twit(config); // importing api keys from separate files
