839c634e8d49f0ab3e770813febd83f0936c2cde
nathan gavin <nathangavin987@gmail.com>
2019-02-26 21:07:29 +1300

    created next tutorial folder

---
+++ tut_4/config_empty.js
@@ -0,0 +1,16 @@
+
+/**
+ * to connect to twitter account, go to dev.twitter.com and create a dev account, then create 
+ * an app and generate the relevant keys. Then create a new file called config.js, and have the 
+ * same content as this file but with the relevant keys filled in.
+ * 
+ */
+
+module.exports = {
+    // consumer_key:         '...',
+    // consumer_secret:      '...',
+    // access_token:         '...',
+    // access_token_secret:  '...',
+    // timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+    // strictSSL:            true,     // optional - requires SSL certificates to be valid.
+}
