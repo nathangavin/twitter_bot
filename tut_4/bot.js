c1d2ba2d18fd8e5dce7f2721c037396d69451a2a
nathan gavin <nathangavin987@gmail.com>
2019-02-26 21:22:31 +1300

    working get call

--- tut_4/bot.js
+++ tut_4/bot.js
@@ -1,8 +1,28 @@
-let Twit = require('twit');
-let config = require('./config');
+const Twit = require('twit'); // ES6 not supported by nodeJS
+const config = require('./config');
 
 console.log("hello");
 
 // making a new Twit object
-// keys are unique to my test account
 let T = new Twit(config); // importing api keys from separate files
+
+
+/**
+ * get request to API
+ * prints the text of the tweets found
+ */
+
+let params = { 
+    q: 'ESO', 
+    count: 2 
+};
+
+function gotData(err, data, response) {
+    let tweets = data.statuses;
+    for (let i = 0; i < tweets.length; i++) {
+        console.log(tweets[i].text);
+    }
+};
+
+T.get('search/tweets', params, gotData);
+
